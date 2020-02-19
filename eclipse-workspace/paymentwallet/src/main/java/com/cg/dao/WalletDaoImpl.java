3e1b0dfd75173a8b07fd726475822718619a9e55
lov <lov@lov-Inspiron-15-3567>
2020-02-19 11:16:13 +0530

    commited by LovaRaju

--- eclipse-workspace/paymentwallet/src/main/java/com/cg/dao/WalletDaoImpl.java
+++ eclipse-workspace/paymentwallet/src/main/java/com/cg/dao/WalletDaoImpl.java
@@ -3,9 +3,24 @@ package com.cg.dao;
 import com.cg.dto.Account;
 import com.cg.dto.Transaction;
 import com.cg.exception.AccountIdException;
+import com.cg.exception.AmountException;
 import com.cg.repo.Repository;
 
 public class WalletDaoImpl implements WalletDao {
+	/*************************************************************************************************************
+	 * @author LovaRaju
+	 * 		Description:The method below returns Account instance if we pass accountId as an argument.If accountId 
+	 * 							present in Repository then it returns an account instance.
+	 * 
+	 * 		Method:The method below returns Account instance if we pass accountId as an argument.If accountId 
+	 * 							present in Repository then it returns an account instance.
+	 * 		@return Account
+	 * 
+	 * 		@param accountId:An accountcId is passed and  fetched in the Repository.
+	 * 
+	 *  	@throws AccountIdException:If the accountId is not available in Repository then AccountIdException is declared.
+	 *****************************************************************************************************************
+	 */
 	public Account getAccountDetails(String accountId) throws AccountIdException {
 		if (Repository.map.containsKey(accountId)) {
 			Account account = Repository.map.get(accountId);
@@ -14,16 +29,39 @@ public class WalletDaoImpl implements WalletDao {
 			throw new AccountIdException("Id does not exist");
 
 	}
+	/*************************************************************************************************************
+	 * @author LovaRaju
+	 * 		Description:The method below returns boolean  if we pass account instance as an argument.It updates the
+	 * 						fromAcc balance and toAcc balance.
+	 * 
+	 * 		Method:The method below returns boolean  if we pass account instance as an argument.It updates the
+	 * 						fromAcc balance and toAcc balance.
+	 * 		@return Boolean
+	 * 
+	 * 		@param acc:An Account instance is passed and  gets updated in the Repository.
+	 * 
+	 *****************************************************************************************************************
+	 */
 
 	public boolean updateAmount(Account acc) {
 		Repository.map.put(acc.getAccountId(), acc);
 		return true;
 
 	}
-
+	/*************************************************************************************************************
+	 * @author LovaRaju
+	 * 		Description:The method below returns boolean  if we pass Transaction instance as an argument.It adds the
+	 * 						transactions as each transfer is successful.
+	 * 		Method:The method below returns boolean  if we pass Transaction instance as an argument.It adds the
+	 * 						transactions as each transfer is successful.
+	 * 
+	 * 		@return Boolean
+	 * 
+	 * 		@param acc:An Transcation instance is passed and  gets added in the Repository.
+	 * 
+	 *****************************************************************************************************************
+	 */
 	
-
-	@Override
 	public boolean addTransaction(Transaction txn) {
 		Repository.map1.put(txn.getTransactionId(), txn);
 			return true;
--- eclipse-workspace/paymentwallet/src/main/java/com/cg/service/PaymentWalletServiceImp.java
+++ eclipse-workspace/paymentwallet/src/main/java/com/cg/service/PaymentWalletServiceImp.java
@@ -1,16 +1,42 @@
 package com.cg.service;
 import java.time.LocalDate;
-
 import com.cg.dao.WalletDaoImpl;
 import com.cg.dto.Account;
 import com.cg.dto.Transaction;
 import com.cg.exception.AccountIdException;
 import com.cg.exception.AmountException;
 import com.cg.repo.Repository;
+/*************************************************************************************************************
+ * @author LovaRaju
+ * 		Description:The class PaymentWalletImp implements Interface PaymentWalletService.The logic of transferring 
+ * 							amount from fromAccId to toAccId is written in this class.
+ * 
+ *****************************************************************************************************************
+ */
 
 public class PaymentWalletServiceImp implements PaymentWalletService {
 	WalletDaoImpl dao = new WalletDaoImpl();
-
+/*************************************************************************************************************
+ * @author LovaRaju
+ * 		Description:The method below returns true if we transfer amount less than available balance and fromAccId,toAccId
+ * 						 are valid arguments present in Repository
+ * 
+ * 		Method:Transfer amount from fromAccID to toAccId if passed parameters are validated against Repository
+ * 
+ * 		@return Boolean
+ * 
+ * 		@param fromAccId:An fromAccId account instance is created and fromAccId is fetched in the Repository.
+ * 
+ * 		@param toAccId:An toAccId account instance is created and toAccID is fetched in the Repository.
+ * 
+ * 		@param amount:If the amount to be transferred is less than available available balance,then amount is transferred
+ * 							from fromAccId to toAccId.
+ * 
+ *  	@throws AmountException:If the amount is negative or amount is greater than available balance then AmountException is declared.
+ *  
+ *  	@throws AccountIdException:If the fromAccId or toAccId are not available in Repository then AccountIdException is declared.
+ *****************************************************************************************************************
+ */
 	public boolean transferFund(String fromAccId,String toAccId,double amount) throws AmountException, AccountIdException
 	{
 		if(amount<0)
